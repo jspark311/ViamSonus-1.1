4aaa96e64a2eb23a2f22b4c8c1b416d65f3827f8
jspark311 <josh.lindsay@gmail.com>
2015-03-16 17:14:57 -0700

    Realized there was Arduino broken-ness left over. Still isn't completely fixed.

--- StaticHub.cpp
+++ StaticHub.cpp
@@ -36,7 +36,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "Drivers/ADCScanner/ADCScanner.h"
 
 
-#include <Encoder/Encoder.h>
+#include "Encoder/Encoder.h"
 
 
 // Externs and prototypes...
--- ViamSonus.ino
+++ ViamSonus.ino
@@ -19,8 +19,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 
 */
+
 #include "StaticHub/StaticHub.h"
 #include "Drivers/AudioRouter/AudioRouter.h"
+#include <Encoder.h>   // Bleh.... more Arduino brain-damage...
 
 #define HOST_BAUD_RATE  115200
 
